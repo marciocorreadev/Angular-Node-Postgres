07a93205bdf3821601f5d22be5339bfe68507fcb
correamarcio <msc@outlook.com.br>
2020-07-12 15:02:57 -0300

    updated project

--- server/database/index.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const dbConfig = require("./config/database");
-
-const ItemPrecos = require("./models/ItemPrecos");
-
-const connection = new Sequelize(dbConfig);
-ItemPrecos.init(connection);
-
-module.exports = connection;
---
+++ web/src/app/precificacao-itens/ItemPreco.ts
@@ -0,0 +1,18 @@
+export interface ItemPreco {
+  id: number;
+  ValorCompra: number;
+  PercCreditoICMS: number;
+  ValorCreditoICMS: number;
+  PercFrete: number;
+  ValorFrete: number;
+  PercIPI: number;
+  ValorIPI: number;
+  PercICMSST: number;
+  ValorICMSST: number;
+  ValorAdicionalCompra: number;
+  PercMargemLucro: number;
+  ValorMargemLucro: number;
+  ValorVenda: number;
+  createdAt: any;
+  updatedAt: any;
+}
